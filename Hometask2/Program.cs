83bb806932a8828ebc735c91de16bba70e79b53d
MamaLilya <mama8lilya93@gmail.com>
2022-02-20 16:46:01 +0300

    HomeWork1

---
+++ Hometask2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Rectangular_parallelepiped
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte x = 0, y = 0, z = 0;
+            Console.Write("Enter the length of the edge x: ");
+            x = Convert.ToByte(Console.ReadLine());
+            Console.Write("Enter the length of the edge y: ");
+            y = Convert.ToByte(Console.ReadLine());
+            Console.Write("Enter the length of the edge y: ");
+            z = Convert.ToByte(Console.ReadLine());
+            byte x_pow = 2;
+            double d = Math.Sqrt(Math.Pow(x, x_pow) + Math.Pow(y, x_pow) + Math.Pow(z, x_pow));
+            Console.Write("The diagonal length of a rectangular parallelepiped is: ");
+            Console.WriteLine(d);
+            Console.ReadKey();
+
+        }
+    }
+}
