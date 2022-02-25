434c76697dcaa247ff77ad379eebae98bce379b6
MamaLilya <mama8lilya93@gmail.com>
2022-02-25 10:31:28 +0300

    Hometask2.3

--- Hometask2/Program.cs
+++ Hometask2/Program.cs
@@ -5,23 +5,34 @@ namespace Rectangular_parallelepiped
     {
         static void Main()
         {
-            byte x = 0, y = 0, z = 0;
+            int x = 0, y = 0, z = 0;
             Console.Write("Enter the length of the edge x: ");
-            x = Convert.ToByte(Console.ReadLine());
+            x = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter the length of the edge y: ");
-            y = Convert.ToByte(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter the length of the edge y: ");
-            z = Convert.ToByte(Console.ReadLine());
-            if x < 0 && y < 0 && z < 0)
-            { 
-                Console.WriteLine("The value cannot be less than 0")
+            z = Convert.ToInt32(Console.ReadLine());
+            if (x <= 0)
+            {
+                Console.WriteLine("The value cannot be less or equal to 0");
             }
-            byte x_pow = 2;
-            double d = Math.Sqrt(Math.Pow(x, x_pow) + Math.Pow(y, x_pow) + Math.Pow(z, x_pow));
-            Console.Write("The diagonal length of a rectangular parallelepiped is: ");
-            Console.WriteLine(d);
-            Console.ReadKey();
+            else if (y <= 0)
+            {
+                Console.WriteLine("The value cannot be less or equal to 0");
+            }
+            else if (z <= 0)
+            {
+                Console.WriteLine("The value cannot be less or equal to 0");
+            }
+            else
+            {
+                byte x_pow = 2;
+                double d = Math.Sqrt(Math.Pow(x, x_pow) + Math.Pow(y, x_pow) + Math.Pow(z, x_pow));
+                Console.Write("The diagonal length of a rectangular parallelepiped is: ");
+                Console.WriteLine(d);
+                Console.ReadKey();
 
+            }
         }
     }
 }
