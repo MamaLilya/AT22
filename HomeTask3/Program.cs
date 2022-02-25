5bc442efc91324cf0c21b5ea03d64722c301655f
MamaLilya <mama8lilya93@gmail.com>
2022-02-25 12:44:39 +0300

    HomeTask3

---
+++ HomeTask3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string xx = ("String XX: "), yy = ("String YY: "), zz = ("String ZZ: ");
+            Console.WriteLine(xx);
+            xx = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(yy);
+            yy = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(zz);
+            zz = Convert.ToString(Console.ReadLine());
+            bool answer = false;
+            if (xx.StartsWith (yy))
+                {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+            Console.WriteLine(answer);
+            if (xx.EndsWith (zz))
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+            Console.WriteLine (answer);
+            Console.ReadKey();
+
+        }
+    }
+}
