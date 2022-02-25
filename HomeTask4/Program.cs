3c43683a1312f695688674a7c8692cd9992243d8
MamaLilya <mama8lilya93@gmail.com>
2022-02-25 13:28:10 +0300

    HomeTask4

---
+++ HomeTask4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Concatenation
+{
+    class Program
+    {
+        static void Main()
+        {
+            string x = ("String1: "), y = ("String2: ");
+            char z = (char)0;
+            Console.WriteLine(x);
+            x = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(y);
+            y = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Symbol: ");
+            z = Convert.ToChar(Console.ReadLine());
+            string phrase = x + y;
+            short LastIndexOf = (short)phrase.LastIndexOf(z);
+            string result = $"Index of {z} in {phrase} = {LastIndexOf}";
+            Console.WriteLine(phrase);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
