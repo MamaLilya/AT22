694c7e49592b6900a3c11484a6ff7479d5bd2c89
MamaLilya <mama8lilya93@gmail.com>
2022-03-29 20:13:47 +0300

    Calculator_1

---
+++ Calculator/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+namespace Calculator
+{ 
+    class Program
+{
+        static void Validation(double a, double b)
+        {
+            if (!double.IsNaN(a))
+            {
+                Console.WriteLine("Enter a number!");
+            }
+            else if (!double.IsNaN(b))
+            {
+                Console.WriteLine("Enter a number!");
+            }
+        }
+        static double Plus (double a, double b)
+        {
+            return a + b;
+        }
+        static double Minus(double a, double b)
+        {
+            return a - b;
+        }
+        static double Product(double a, double b)
+        {
+            return a * b;
+        }
+        static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+        static double Grade(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+        static void Main()
+        {
+            double a = 0, b = 0;
+            Console.WriteLine("Hello! This is a calculator! Here you can sum, deduct, increase, divide or exponentiate two numbers!");
+            Console.WriteLine("Choose an arithmetic action: " +
+                "Plus - 1,  " +
+                "Minus - 2, " +
+                "Product - 3, " +
+                "Divide - 4, " +
+                "Grade - 5. ");
+            string c = Console.ReadLine();
+            Console.WriteLine($"Enter first number: ");
+            a = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            b = byte.Parse(Console.ReadLine());
+            Validation(a, b);
+            if (c == "1")
+            {
+                Console.WriteLine($"Your result is {Plus(a, b)}");
+            }
+            else if (c == "2")
+            {
+                Console.WriteLine($"Your result is {Minus(a, b)}");
+            }
+            else if (c == "3")
+            {
+                Console.WriteLine($"Your result is {Product(a, b)}");
+            }
+            else if (c == "4")
+            {
+                Console.WriteLine($"Your result is {Divide(a, b)}" );
+            }
+            else if (c == "5")
+            {
+                Console.WriteLine($"Your result is {Grade(a, b)}");
+            }
+                    
+                Console.ReadKey();
+            }
+        }
+}
