90566b2b8b89697b1e50744ee67795f02f97b810
MamaLilya <mama8lilya93@gmail.com>
2022-03-30 19:08:13 +0300

    Calculator with validation of arithmetic actions

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 namespace Calculator
-{ 
-    class Program
 {
-        static double Plus (double a, double b)
+    class Program
+    {
+        static double Plus(double a, double b)
         {
             return a + b;
         }
@@ -23,43 +23,57 @@ namespace Calculator
         {
             return Math.Pow(a, b);
         }
+        static string Validation(string c)
+        {
+            while (c != "+" & c != "-" & c != "*" & c != "/" & c != "^")
+            {
+                Console.WriteLine("Wrong!");
+                Console.WriteLine("Choose an arithmetic action: ");
+                c = Console.ReadLine();
+                if (c == "+" || c == "-" || c == "*" || c == "/" || c == "^")
+                {
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return c;
+        }
         static void Main()
         {
             double a = 0, b = 0;
             Console.WriteLine("Hello! This is a calculator! Here you can sum, deduct, increase, divide or exponentiate two numbers!");
-            Console.WriteLine("Choose an arithmetic action: " +
-                "Plus - 1,  " +
-                "Minus - 2, " +
-                "Product - 3, " +
-                "Divide - 4, " +
-                "Power - 5. ");
-            string c = Console.ReadLine();
             Console.WriteLine($"Enter first number: ");
             a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Choose an arithmetic action: ");
+            string c = Console.ReadLine();
+            c = Validation(c);
             Console.WriteLine("Enter second number: ");
             b = double.Parse(Console.ReadLine());
-            if (c == "1")
+            if (c == "+")
             {
                 Console.WriteLine($"Your result is {Plus(a, b)}");
             }
-            else if (c == "2")
+            else if (c == "-")
             {
                 Console.WriteLine($"Your result is {Minus(a, b)}");
             }
-            else if (c == "3")
+            else if (c == "*")
             {
                 Console.WriteLine($"Your result is {Product(a, b)}");
             }
-            else if (c == "4")
+            else if (c == "/")
             {
-                Console.WriteLine($"Your result is {Divide(a, b)}" );
+                Console.WriteLine($"Your result is {Divide(a, b)}");
             }
-            else if (c == "5")
+            else if (c == "^")
             {
                 Console.WriteLine($"Your result is {Power(a, b)}");
             }
-                    
-                Console.ReadKey();
-            }
+
+            Console.ReadKey();
         }
+    }
 }
