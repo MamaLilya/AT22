1106e08d49b011a9bed02fa63aff03374d8eb6c8
MamaLilya <mama8lilya93@gmail.com>
2022-05-06 18:44:17 +0300

    Authenticator1.0

---
+++ Authenticator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Authenticator
+{
+    class Program
+    {
+        static string AskUser(string var)
+        {
+            Console.Write($"Enter {var}: ");
+            return Console.ReadLine();
+        }
+        static void Main()
+        {
+            int counter = 0;
+            while (counter < 3)
+            {
+                string username = AskUser("Username");
+                string password = AskUser("Password");
+                if (username == "admin" && password == "adminadmin")
+                {
+                    Console.WriteLine("Welcome!");
+                    break;
+                }
+
+                else
+                {
+                    counter++;
+                    Console.WriteLine($"Wrong Username or Password. You have 3 attempts and there are {3 - counter} attempts left.");
+                }
+            }
+
+                Console.ReadKey();
+
+
+            
+            }
+    }
+}
